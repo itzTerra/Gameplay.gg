d25adb8cb6988a66278a0af33db2666357cadb62
Michal Doležel <michaldolezel123@gmail.com>
2023-07-30 17:13:14 +0200

    better credential storage

--- server/utils/firebase.ts
+++ server/utils/firebase.ts
@@ -2,8 +2,12 @@ import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from "firebase-admin/auth"
 
+
+// Get the Firebase Admin credentials from environment variables
+const firebaseAdminCredentials = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
+
 export const firebaseAdminApp = initializeApp({
-    credential: cert("./service-account.json"),
+    credential: cert(firebaseAdminCredentials),
 })
 
 export const adminAuth = getAuth
